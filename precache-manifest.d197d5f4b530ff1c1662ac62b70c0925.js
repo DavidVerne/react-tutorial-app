d5601ade685b6f2eff683809f38db71d8003bca9
David Verne <davidverne@Susans-MacBook-Air.local>
2018-11-23 13:19:13 +0000

    Updates

---
+++ precache-manifest.d197d5f4b530ff1c1662ac62b70c0925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "924ef6789eb6ac9bd772",
+    "url": "/react-tutorial-app/static/js/runtime~main.924ef678.js"
+  },
+  {
+    "revision": "a9a9893112ab4bf51227",
+    "url": "/react-tutorial-app/static/js/main.a9a98931.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/react-tutorial-app/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "a9a9893112ab4bf51227",
+    "url": "/react-tutorial-app/static/css/main.635f87f7.chunk.css"
+  },
+  {
+    "revision": "5cbeff53021da21334e322614e43db7c",
+    "url": "/react-tutorial-app/index.html"
+  }
+];
